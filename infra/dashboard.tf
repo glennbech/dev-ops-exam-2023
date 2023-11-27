ad96702323b0a9547a133b3c89d70333ba9c202d
EC2 Default User <ec2-user@ip-172-31-41-35.eu-west-1.compute.internal>
2023-11-27 16:34:51 +0000

    Added second widget for counting legal faces

--- infra/dashboard.tf
+++ infra/dashboard.tf
@@ -21,7 +21,26 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Metric for PPE"
       }
-    }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.prefix}",
+            "good_face.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Number of PPE approved faces"
+      }
+     }
   ]
 }
 DASHBOARD
